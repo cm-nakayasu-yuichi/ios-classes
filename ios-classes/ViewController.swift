edff302cfe2ee5e1d50af5933fa866a61fe4e786
Yuichi NAKAYASU <nakayasu.yuichi@classmethod.jp>
2021-10-14 16:20:52 +0900

    add bootstrap-view-controller

--- ios-classes/ViewController.swift
+++ ios-classes/ViewController.swift
@@ -2,7 +2,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private typealias Section = (String, [Row])
+    private typealias Row = (String, (UIViewController) -> ())
+    
+    private let sections: [Section] = [
+        ("テスト", [
+            ("テスト", { this in
+                print(1)
+            }),
+        ]),
+    ]
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        return section.1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = sections[indexPath.section].1[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "row", for: indexPath)
+        cell.textLabel?.text = row.0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let row = sections[indexPath.section].1[indexPath.row]
+        row.1(self)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].0
     }
 }
