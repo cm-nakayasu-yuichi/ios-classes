c86a326b5a7b9c81266917667d44ca1612465757
Yuichi NAKAYASU <nakayasu.yuichi@classmethod.jp>
2021-10-15 15:41:14 +0900

    CoreGraphics系の拡張

--- ios-classes/Classes/CoreGraphics+Extension.swift
+++ ios-classes/Classes/CoreGraphics+Extension.swift
@@ -4,16 +4,33 @@ import CoreGraphics
 // MARK: - CGRect
 extension CGRect {
     
+    init(_ width: CGFloat, _ height: CGFloat, _ x: CGFloat, _ y: CGFloat) {
+        self.init(x: x, y: y, width: width, height: height)
+    }
+    
+    init(_ origin: CGPoint, _ size: CGSize) {
+        self.init(origin: origin, size: size)
+    }
+    
+    init(_ size: CGSize) {
+        self.init(origin: .zero, size: size)
+    }
 }
 
 // MARK: - CGPoint
 extension CGPoint {
     
+    init(_ x: CGFloat, _ y: CGFloat) {
+        self.init(x: x, y: y)
+    }
 }
 
 // MARK: - CGSize
 extension CGSize {
     
+    init(_ width: CGFloat, _ height: CGFloat) {
+        self.init(width: width, height: height)
+    }
 }
 
 // MARK: - CGFloat
