85bd9e5e084a280f4a43a71b83a74db2f144c21c
Yuichi NAKAYASU <nakayasu.yuichi@classmethod.jp>
2023-02-15 16:30:47 +0900

    動作確認用画面(BootstrapViewController)を追加

---
+++ iOS-Classes/Sources/ViewControllers/BootstrapViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class BootstrapViewController: UIViewController {
+
+    private typealias Section = (String, [Row])
+    private typealias Row = (String, (UIViewController) -> ())
+    
+    private let sections: [Section] = [
+        ("テスト", [
+            ("テスト", { this in
+                print(1)
+            }),
+        ]),
+    ]
+}
+
+extension BootstrapViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        return section.1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = sections[indexPath.section].1[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "row", for: indexPath)
+        cell.textLabel?.text = row.0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let row = sections[indexPath.section].1[indexPath.row]
+        row.1(self)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].0
+    }
+}
